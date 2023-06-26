f76a9304ace677f691f1f4fe2c4563abb72c4b35
MengHao Tian <bluetian1st@gmail.com>
2023-06-26 21:25:03 +0800

    add images and mindMaps

--- mindMaps/线性代数.mm
+++ mindMaps/线性代数.mm
@@ -2,8 +2,8 @@
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="线性代数" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1687132700562" VGAP_QUANTITY="3 pt">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<hook NAME="MapStyle" background="#fbf1c7">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_gruvbox_template.mm"/>
+<hook NAME="MapStyle" background="#fbf1c7" zoom="0.832" layout="OUTLINE">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_gruvbox_template.mm" fit_to_viewport="false" allow_compact_layout="false"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
@@ -76,6 +76,100 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="" POSITION="bottom_or_right" ID="ID_1412537661" CREATED="1687132701875" MODIFIED="1687132701875"/>
+<node TEXT="二次型" POSITION="bottom_or_right" ID="ID_840493615" CREATED="1687162383507" MODIFIED="1687760731280">
+<node TEXT="定义" ID="ID_461058357" CREATED="1687760732852" MODIFIED="1687760794512">
+<node TEXT="\latex &#xa;$$&#xa;n元变量x_1,x_2,\cdots,x_n 的二次齐次多项式 \\&#xa;f(x_1,x_2,\cdots,x_n)= \\&#xa;a_{11}x_1^2 + 2a_{12}x_1x_2+\cdots+2a_{1n}x_1x_n+a_{22}x_2^2+\cdots+2a_{2n}x_2x_n+\cdots+a_{nm}x_n^2 \\&#xa;称为n元二次型,简称二次型&#xa;$$" ID="ID_1669141417" CREATED="1687760795213" MODIFIED="1687761566871"/>
+</node>
+<node TEXT="矩阵表示" ID="ID_1359981129" CREATED="1687760782729" MODIFIED="1687761645926">
+<node TEXT="定义" ID="ID_1694376721" CREATED="1687772524177" MODIFIED="1687780279064" VGAP_QUANTITY="3 pt">
+<node TEXT="\latex&#xa;$$&#xa;因为x_ix_j=x_jx_i ,令a_{ij}=a_{ji} ,则2a_{ij}x_ix_j=a_{ij}x_ix_j+a_{ji}x_jx_i ,于是  \\&#xa;f(x_1 ,x_2 ,\cdots , x_n ) =  \sum_{i=1}^n \sum_{j=1}^n a_{ij}x_ix_j \ ,\\&#xa;矩阵A = \left[\begin{array}\\&#xa;a_{11}  &amp;  a_{12}  &amp;  \cdots  &amp;  a_{1n}  \\&#xa;a_{21}  &amp;  a_{22}  &amp;  \cdots  &amp;  a_{2n}  \\&#xa;\vdots  &amp;  \vdots   &amp;  \   &amp;  \vdots  \\&#xa;a_{n1}  &amp;  a_{n2}  &amp;  \cdots  &amp;  a_{nn}&#xa;\end{array}\right]  \\&#xa;为二次型的矩阵&#xa;$$" ID="ID_1322524247" CREATED="1687761752103" MODIFIED="1687780279063"/>
+</node>
+<node TEXT="性质" ID="ID_5570803" CREATED="1687772452897" MODIFIED="1687772455395">
+<node TEXT="\latex $$是对称矩阵(满足A^T=A)$$" ID="ID_1138189060" CREATED="1687772473601" MODIFIED="1687772564611"/>
+<node TEXT="唯一" ID="ID_316880011" CREATED="1687772456055" MODIFIED="1687772492540"/>
+<node TEXT="矩阵的秩称为二次型f(x)的秩" ID="ID_1222234564" CREATED="1687772500392" MODIFIED="1687772516065"/>
+</node>
+</node>
+<node TEXT="化二次型为标准型与规范型" ID="ID_1424881070" CREATED="1687768228866" MODIFIED="1687779586113" VGAP_QUANTITY="3 pt">
+<node TEXT="可逆线性变换" POSITION="bottom_or_right" ID="ID_120985328" CREATED="1687768839155" MODIFIED="1687768851971">
+<node TEXT="\latex&#xa;$$&#xa;对于n元二次型f(x_1,x_2,\cdots,x_n),若令\\&#xa;\left\{\begin{matrix}&#xa;x_1=c_{11}y_1+c_{12}y_2+\cdots+c_{1n}y_n\,,\\&#xa;x_2=c_{21}y_1+c_{22}y_2+\cdots+c_{2n}y_n\,,\\&#xa;\cdots \cdots \\&#xa;x_n=c_{n1}y_1+c_{n2}y_2+\cdots+c_{nn}y_n\,,\\&#xa;\end{matrix}\right. \\&#xa;记x=\begin{bmatrix}x_1 \\ x_2 \\ \vdots \\ x_n \end{bmatrix},&#xa;C=\begin{bmatrix}&#xa;c_{11} &amp; c_{12} &amp; \cdots &amp; c_{1n} \\&#xa;c_{21} &amp; c_{22} &amp; \cdots &amp; c_{2n} \\&#xa;\vdots &amp; \vdots &amp; \, &amp; \vdots \\&#xa;c_{n1} &amp; c_{n2} &amp; \cdots &amp; c_{nn}&#xa;\end{bmatrix},&#xa;y=\begin{bmatrix}y_1 \\ y_2 \\ \vdots \\ y_n \end{bmatrix},\\&#xa;则原二次型多项式可写为 \\&#xa;x=Cy \\&#xa;若线性变换的系数矩阵C可逆(C \neq 0),称之为可逆线性变换&#xa;$$" ID="ID_403931447" CREATED="1687768852863" MODIFIED="1687769690809"/>
+<node TEXT="\latex&#xa;$$&#xa;给出f(x)=x^TAx,令x=Cy,\\&#xa;则f(x)=(Cy)^TA(Cy)=y^T(C^TAC)y,\\&#xa;记B=C^TAC,则f(x)=y^TBy=g(y),\\&#xa;此时称二次型f(x)=x^TAx通过线性变换x=Cy\\&#xa;得到一个新的二次型g(y)=y^TBy&#xa;$$" ID="ID_1033030445" CREATED="1687769722749" MODIFIED="1687770061266"/>
+</node>
+<node TEXT="标准型" POSITION="bottom_or_right" ID="ID_1018483313" CREATED="1687772184725" MODIFIED="1687772606487">
+<node TEXT="只含有平方项，没有交叉项(所有交叉项的系数全为0)的二次型" ID="ID_1417894090" CREATED="1687772612503" MODIFIED="1687772861797"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      \latex $$形如d_1x_1^2+d_2x_2^2+\cdots+d_nx_n^2$$
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="规范型" POSITION="bottom_or_right" ID="ID_1125673814" CREATED="1687772606950" MODIFIED="1687772681688">
+<node TEXT="标准型中，系数仅为1,-1,0的二次型" ID="ID_1685787015" CREATED="1687772676172" MODIFIED="1687772925425"><richcontent CONTENT-TYPE="xml/" TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      \latex $$形如x_1^2+\cdots+x_p^2-x_{p+1}^2-\cdots-x_{p+q}^2$$
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="合同" ID="ID_1730441697" CREATED="1687768256096" MODIFIED="1687779586112">
+<node TEXT="定义" ID="ID_1634106903" CREATED="1687768478139" MODIFIED="1687768482252">
+<node TEXT="\latex&#xa;$$&#xa;设A,B为n阶矩阵，若存在可逆矩阵C(|C|\neq0),使得C^TAC=B,则称A与B合同,记作A \simeq B.&#xa;$$" ID="ID_846963010" CREATED="1687768278863" MODIFIED="1687770152540"/>
+</node>
+<node TEXT="性质" ID="ID_74200349" CREATED="1687768483582" MODIFIED="1687768501610">
+<node TEXT="\latex $$ A \simeq A反身性 $$" ID="ID_320350640" CREATED="1687768549553" MODIFIED="1687768591466"/>
+<node TEXT="\latex $$ 若A \simeq B,则B \simeq A 对称性 $$" ID="ID_1721846096" CREATED="1687768599501" MODIFIED="1687768632541"/>
+<node TEXT="\latex $$ 若A \simeq B ,且B \simeq C,则A \simeq C 传递性 $$" ID="ID_485664754" CREATED="1687768633754" MODIFIED="1687768677376"/>
+</node>
+<node TEXT="推论" ID="ID_1840334205" CREATED="1687771453448" MODIFIED="1687778489662">
+<node TEXT="\latex&#xa;$$&#xa;若A \simeq B,则r(A)=r(B) \\&#xa;\rightarrow 可逆线性变换不会改变二次型的秩&#xa;$$" ID="ID_1992723469" CREATED="1687771458008" MODIFIED="1687771562338"/>
+<node TEXT="\latex&#xa;$$&#xa;和对称矩阵(A^T=A)合同的矩阵也必是对称矩阵, \\&#xa;因为若A \simeq B,\exists可逆矩阵C,使得C^TAC=B, \\&#xa;则B^T=(C^TAC)^T=C^TA^TC=C^TAC=B&#xa;$$" ID="ID_1951529805" CREATED="1687771574684" MODIFIED="1687771793921"/>
+</node>
+<node TEXT="合同标准型" ID="ID_833443897" CREATED="1687777933173" MODIFIED="1687777941464">
+<node TEXT="\latex&#xa;$$&#xa;二次型f(x)=x^TAx合同于标准型d_1x_1^2+d_2x_2^2+\cdots+d_nx_n^2,\\&#xa;则称d_1x_1^2+d_2x_2^2+\cdots+d_nx_n^2为f(x)的合同标准型&#xa;$$" ID="ID_421883735" CREATED="1687777947698" MODIFIED="1687778050667"/>
+</node>
+<node TEXT="合同规范型" ID="ID_1571194856" CREATED="1687777941667" MODIFIED="1687777946872">
+<node TEXT="\latex&#xa;$$&#xa;二次型f(x)=x^TAx合同于规范型\\&#xa;x_1^2+\cdots+x_p^2-x_{p+1}^2-\cdots-x_{p+q}^2,\\&#xa;则称x_1^2+\cdots+x_p^2-x_{p+1}^2-\cdots-x_{p+q}^2为f(x)的合同规范型&#xa;$$" ID="ID_1555941941" CREATED="1687778052503" MODIFIED="1687778122125"/>
+</node>
+</node>
+<node TEXT="配方法" ID="ID_653303825" CREATED="1687768247195" MODIFIED="1687768251013">
+<node TEXT="\latex&#xa;$$&#xa;任何实对称矩阵A,必存在可逆矩阵C,使得C^TAC=\Lambda,其中\\&#xa;\Lambda=\begin{bmatrix}&#xa;d_1 &amp; \, &amp; \, &amp; \, \\&#xa;\, &amp; d_2 &amp; \, &amp; \, \\&#xa;\, &amp; \, &amp; \ddots &amp; \, \\&#xa;\, &amp; \, &amp; \, &amp; d_n&#xa;\end{bmatrix} (标准型)或 \\&#xa;\Lambda=\begin{bmatrix}&#xa;1  &amp;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp; \\&#xa;  &amp; \ddots &amp;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp; \\&#xa;  &amp;  &amp; 1 &amp;  &amp;  &amp;  &amp;  &amp;  &amp; \\&#xa;  &amp;  &amp;  &amp; -1 &amp;  &amp;  &amp;  &amp;  &amp; \\&#xa;  &amp;  &amp;  &amp;  &amp; \ddots &amp;  &amp;  &amp;  &amp; \\&#xa;  &amp;  &amp;  &amp;  &amp;  &amp; -1 &amp;  &amp;  &amp; \\&#xa;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp; 0 &amp;  &amp; \\&#xa;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp; \ddots &amp; \\&#xa;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp;  &amp; 0&#xa;\end{bmatrix} (规范型)&#xa;$$" ID="ID_655460305" CREATED="1687778645220" MODIFIED="1687779115225"/>
+</node>
+<node TEXT="正交变换法" ID="ID_1624809382" CREATED="1687768251503" MODIFIED="1687768255882">
+<node TEXT="\latex&#xa;$$&#xa;任何实对称矩阵A,一定存在正交矩阵Q, \\&#xa;使得Q^{-1}AQ=Q^TAQ=\Lambda,其中\\&#xa;\Lambda=\begin{bmatrix}&#xa;\lambda_1 &amp;  &amp; \\&#xa;  &amp; \lambda_2 &amp;  &amp; \\&#xa;  &amp;  &amp; \ddots &amp; \\&#xa;  &amp;  &amp;  &amp; \lambda_n&#xa;\end{bmatrix}&#xa;$$" ID="ID_357321949" CREATED="1687779134979" MODIFIED="1687779278198"/>
+</node>
+<node TEXT="惯性定理" ID="ID_250387577" CREATED="1687768258737" MODIFIED="1687768262805">
+<node TEXT="定义" ID="ID_1219970489" CREATED="1687779343632" MODIFIED="1687779352374">
+<node TEXT="无论选取什么样的可逆线性变换，将二次型化为标准型或规范型，其正项个数p,负项个数q都是不变的，p称为正惯性指数，q称为负惯性指数" ID="ID_1972745681" CREATED="1687779352732" MODIFIED="1687779451945"/>
+</node>
+<node TEXT="推论" ID="ID_202656995" CREATED="1687779454681" MODIFIED="1687779457468">
+<node TEXT="若二次型的秩为r，则r=p+q" ID="ID_1864815843" CREATED="1687779457669" MODIFIED="1687779482808"/>
+<node TEXT="两个二次型（或实对称矩阵）合同的充要条件是有相同的正、负惯性指数，或有相同的秩及正（或负）惯性指数" ID="ID_1807129851" CREATED="1687779483606" MODIFIED="1687779549261"/>
+</node>
+</node>
+</node>
+<node TEXT="正定二次型" ID="ID_403952524" CREATED="1687772048004" MODIFIED="1687780239603" VGAP_QUANTITY="3 pt">
+<node TEXT="定义" ID="ID_957693595" CREATED="1687779593806" MODIFIED="1687779595956">
+<node TEXT="\latex&#xa;$$&#xa;n元二次型f(x_1,x_2,\cdots,x_n)=x^TAx,\\&#xa;若对任意的x=[x_1,x_2,\cdots,x_n]^T\neq0,均有x^TAx&gt;0,则称f为正定二次型,称二次型的矩阵A为正定矩阵&#xa;$$" ID="ID_1556426278" CREATED="1687779625567" MODIFIED="1687779745835"/>
+</node>
+<node TEXT="充要条件" ID="ID_1550907217" CREATED="1687779596437" MODIFIED="1687780239603">
+<node TEXT="\latex&#xa;$$&#xa;n元二次型f=x^TAx正定\\&#xa;\Longleftrightarrow 对任意的x\ne 0,有x^TAx&gt;0(定义)\\&#xa;\Longleftrightarrow f的正惯性指数p=n\\&#xa;\Longleftrightarrow \exists 可逆矩阵D,使A=D^TD\\&#xa;\Longleftrightarrow A\simeq E\\&#xa;\Longleftrightarrow A的特征值\lambda_i&gt;0(i=1,2,\cdots ,n)\\&#xa;\Longleftrightarrow A的全部顺序主子式均大于0&#xa;$$" ID="ID_1268334963" CREATED="1687780091027" MODIFIED="1687780144656"/>
+</node>
+<node TEXT="必要条件" ID="ID_975700221" CREATED="1687779613129" MODIFIED="1687779618693">
+<node TEXT="\latex&#xa;$$&#xa;(1) a_{ii}&gt;0(i=1,2,\cdots,n)\\&#xa;(2) |A|&gt;0&#xa;$$" ID="ID_1025276756" CREATED="1687780157246" MODIFIED="1687780229657"/>
+</node>
+</node>
+</node>
 </node>
 </map>
